e6b867e3b6034d02e35d0d07710fc925ab1b4086
placeforyourads <zvpltpen139763@gmail.com>
2025-11-28 15:36:37 +0300

    2nd commit

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import cv2
-import numpy
-import mediapipe
+import numpy as np
+import mediapipe as mp
+mph = mp.solutions.hands
+mpdr = mp.solutions.drawing_utils
+hands = mph.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.8)
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
+        break
+    flipped = np.fliplr(frame)
+    # переводим его в формат RGB для распознавания
+    flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
+    # Распознаем
+    results = hands.process(flippedRGB)
+    if results.multi_hand_landmarks is not None:
+        mpdr.draw_landmarks(flippedRGB, results.multi_hand_landmarks[0], mph.HAND_CONNECTIONS)
+        try:
+            mpdr.draw_landmarks(flippedRGB, results.multi_hand_landmarks[1], mph.HAND_CONNECTIONS)
+        except Exception:
+            pass
+    res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
+    cv2.imshow("Hands", res_image)
+
+# освобождаем ресурсы
+hands.close()
